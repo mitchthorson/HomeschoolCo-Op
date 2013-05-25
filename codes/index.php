e580ffca63c7a535ad50b636164fad621f487e45
Mitchell Thorson <mitvok89@gmail.com>
2013-05-25 14:40:01 -0400

    changing styles in codes index

--- codes/index.php
+++ codes/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link href="codes.css" rel="stylesheet" type="text/css" /> 
+<link rel="stylesheet" type="text/css" href="./styles/style.css">
 
 <title>Download the Album!</title>
 </head>
